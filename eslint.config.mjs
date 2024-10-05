ac202363a53534ac17b67c06135605e0449db938
nikameru <nika.ra.txt@gmail.com>
2024-10-05 23:52:56 +0300

    Ignore unnecessary folders in eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,14 +4,16 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
+	{
+		ignores: [
+			"**/dist/**",
+			"**/build/**",
+			"**/node_modules/**"
+		]
+	},
 	eslint.configs.recommended,
 	...tseslint.configs.recommended,
 	{
-		ignores: [
-			"dist",
-			"build",
-			"node_modules"
-		],
 		rules: {
 			"@typescript-eslint/no-explicit-any": "off",
 			"@typescript-eslint/no-namespace": "off"
